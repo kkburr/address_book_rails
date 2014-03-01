01ab24841ecaffbfd65fd377a51ea3841a41ffd6
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-03-01 14:04:24 -0700

    Push at the end of class

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,11 @@
 class City < ActiveRecord::Base
+  validates :elevation, numericality: {
+    :greater_than_or_equal_to => 3315,
+    #:if => lambda {|city| city.state = "CO"},
+    :if => 'is_colorado?'
+    :message => "is too low, lowest point in Colorado is 3315"
+  }
+  def is_colorado?
+    state == "CO"
+  end
 end
