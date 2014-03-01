d1df167453f382ab2c8691f78048742ea5b1d9e4
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-03-01 14:09:49 -0700

    Another Class Commit

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,11 +1,7 @@
 class City < ActiveRecord::Base
   validates :elevation, numericality: {
     :greater_than_or_equal_to => 3315,
-    #:if => lambda {|city| city.state = "CO"},
-    :if => 'is_colorado?'
+    :if => lambda {|city| city.state = "CO"},
     :message => "is too low, lowest point in Colorado is 3315"
   }
-  def is_colorado?
-    state == "CO"
-  end
 end
