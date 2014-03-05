2f8c4fdc8cdcc9e901283dd0ad7f4b48f3a17310
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-03-05 09:25:35 -0700

    Homework Commit

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,11 @@
 class City < ActiveRecord::Base
+  validates :name, :population, :elevation, :state, presence: true
   validates :elevation, numericality: {
-    :greater_than_or_equal_to => 3315,
-    :if => lambda {|city| city.state = "CO"},
-    :message => "is too low, lowest point in Colorado is 3315"
+      :greater_than_or_equal_to => 3315,
+      :if => lambda {|city| city.state == "CO"},
+      :message => "is too low, lowest point in Colorado is 3315"
+  }
+  validates :population, numericality: {
+      :greater_than => 0
   }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,17 @@
 class Person < ActiveRecord::Base
-  validates :first_name, presence: true
+
+  validates :first_name, :last_name, :email, :address_line_one, :city, :state, :zip_code, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  validates :zip_code, length: { minimum: 5 },
+            format: { with: /\d{5}/ }
+
+  validates :state, inclusion: {
+      :in => %w(AK KY NY AL LA OH AR MA OK AZ MD OR CA  ME PA CO MI RI CT MN SC DC MO SD DE MS TN
+ +FL MT TX GA NC UT HI ND VA IA NE VT ID NH WA IL NJ WI IN NM WV KS NV WY),
+      :message => "is not a valid state" }
+
 end
